72efad4d5448db8e3d1b64b320363936dc51d65b
Cristiano Silva <cristianosstec@gmail.com>
2021-02-06 09:50:04 -0300

    Change services message

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,22 @@ WORKDIR=.
 cd $WORKDIR/output
 
 echo Starting c_service...
-./c_service 1111 /c_service "C Service Replying..."&
+./c_service 1111 /c_service "C Service Replying: Hello World!"&
 
 echo Starting go_service ...
-./go_service 1112 /go_service "Go Service Replying..."&
+./go_service 1112 /go_service "Go Service Replying: Hallo Welt"&
 
 echo Starting python_service ...
-./python_service.py 1113 /python_service "Python Service Replying..."&
+./python_service.py 1113 /python_service "Python Service Replying: salve Orbis Terrarum"&
 
 echo Starting ruby_service ...
-./ruby_service.rb 1114 /ruby_service "Ruby Service Replying..."&
+./ruby_service.rb 1114 /ruby_service "Ruby Service Replying: Bonjour le monde"&
 
 echo Starting rust_service ...
-./rust_service 1115 /rust_service "Rust Service Replying..."&
+./rust_service 1115 /rust_service "Rust Service Replying: Ciao mondo"&
 
 echo Starting java_service ...
-java UDPServer 1116 /java_service "Java Service Replying..."&
+java UDPServer 1116 /java_service "Java Service Replying: Olá Mundo"&
 
 echo Starting Gateway ....
 sleep 1
